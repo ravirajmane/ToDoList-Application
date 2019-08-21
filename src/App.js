02671c031d4cf71b9286969473d75aa835e61d6b
ravirajmane <myemail@gmail.com>
2019-08-20 23:05:32 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      val:'',
+      list: [],
+    };
+  }
+
+
+  add = element => {
+    let val=element.target.value
+    this.setState({
+     val
+    })
+   }
+ 
+   onAddItem=()=> {
+     let item=this.state.val,
+       list=this.state.list;
+       list.push(item);
+ 
+     this.setState({
+       list
+     })
+   }
+   updateExistingItem=i=>{
+    let valupdate=i.target.value
+    
+    this.setState({
+      value_update:valupdate
+    })
+   }
+   onUpdateItem = i => {
+      let item1=this.state.value_update;
+      console.log(item1);
+      let list=this.state.list;
+
+          let list1 = list.map((item, j) => {
+            if (j === i) {
+            return item+1;
+            } else {
+              return item;
+            }
+          });
+
+          this.setState({
+            list1
+          })
+  }
+
+  onRemoveItem = i => {
+    let item=this.state.list;
+
+     const list=item.filter((item,j)=>i !== j);
+ 
+     this.setState({
+       list
+     })
+  };
+
+  render() {
+    let val1=this.state.val;
+    let list1= this.state.list;
+    let newvalupdate=this.state.value_update;
+    return (
+      <div>
+        <ul>
+          {list1.map((item, index) => (
+            <li key={index}>{item}
+             <button onClick={this.onRemoveItem.bind(this,index)}>Remove</button>
+             <button onClick={this.onUpdateItem.bind(this,index)}> Update Item</button>
+            <input type="text" placeholder="Update item here" value={newvalupdate} onChange={this.updateExistingItem}/>
+              <br></br>
+        
+              </li>
+          ))}
+
+              <input type="text" value={val1} onChange={this.add}/>
+              <br></br>
+              <button onClick={this.onAddItem}>Add</button>
+
+             
+              <br></br>
+              
+
+
+
+
+           
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
